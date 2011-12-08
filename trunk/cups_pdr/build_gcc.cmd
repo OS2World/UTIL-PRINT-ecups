468bb225d08c44cf089779be260597d71958f5c7
ataylor <ataylor@2100533d-4a56-dc11-ae90-00e081727c95>
2011-12-08 09:43:33 +0000

    Import of CUPS.PDR sources.

---
+++ trunk/cups_pdr/build_gcc.cmd
@@ -0,0 +1,7 @@
+del cups.o
+del cups.pdr
+cmd /c makedesc -Rcups.def -N"netlabs.org" -V"#define=DRIVER_VERSION,cups.h" cups.def
+gcc -o cups.o cups.c -c
+RC -r cups.RC cups.RES
+gcc -Zdll -Zbin-files -Zomf -o cups.pdr cups.o cups.def cups.res
+ea2 -e DEFAULT_PORT=CUPS cups.pdr
