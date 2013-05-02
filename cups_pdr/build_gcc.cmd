002e0fb8ee7e69320d37aa327efa402a9aa2b8d5
Alex Taylor <altsan@users.noreply.github.com>
2013-05-02 12:59:47 +0000

    Fix BLDLEVEL description (GCC build, not tested)

--- cups_pdr/build_gcc.cmd
+++ cups_pdr/build_gcc.cmd
@@ -1,6 +1,6 @@
 del cups.o
 del cups.pdr
-cmd /c makedesc -Rcups.def -N"netlabs.org" -V"#define=DRIVER_VERSION,cups.h" cups.def
+cmd /c makedesc -R"CUPS port driver" -N"netlabs.org" -V"#define=DRIVER_VERSION,cups.h" cups.def
 gcc -o cups.o cups.c -c
 RC -r cups.RC cups.RES
 gcc -Zdll -Zbin-files -Zomf -o cups.pdr cups.o cups.def cups.res
